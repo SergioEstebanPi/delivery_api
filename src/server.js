f72c748aa4cadc866ee67162e8462c4115e4ddee
Sergio <sepi_147@hotmail.com>
2022-01-10 05:08:57 -0500

    Creating first route

--- src/server.js
+++ src/server.js
@@ -2,10 +2,30 @@ const express = require('express')
 const app = express()
 const http = require('http')
 const server = http.createServer(app)
+const logger = require('morgan')
+const cors = require('cors')
 
 const port = process.env.PORT || 3000
+
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(cors())
+
+app.disable('x-powered-by')
+
 app.set('port', port)
 
 server.listen(port, '192.168.189.41' || 'localhost', function() {
     console.log('server running ' + port + " iniciado")
+})
+
+app.get('/', (req, res) => {
+    res.send('Ruta raiz del backend')
+})
+
+// error handler
+app.use((err, req, res, next) => {
+    console.log(err)
+    res.status(err.status || 500).send(err.stack)
 })
